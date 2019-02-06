bf6946112d81f5a724f13df1589910f66df36e98
Leyla <lisana5632@gmail.com>
2019-02-06 16:02:48 +0300

    Added added tokenizer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "word.h"
-
+#include "tokenizer.h"
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
-  return freelingAddon::Word::Init(env, exports);
+  freelingAddon::WrappedWord::Init(env, exports);
+	return freelingAddon::WrappedTokenizer::Init(env, exports);
 }
 
 NODE_API_MODULE(freeling, InitAll)
-
