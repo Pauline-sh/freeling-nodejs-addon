7c678c2a375f20303cbce7d54a94f2c2a0657539
Leyla <lisana5632@gmail.com>
2019-02-12 15:41:33 +0300

    getters moved to public

--- include/word.h
+++ include/word.h
@@ -17,37 +17,29 @@ namespace freelingAddon {
         static Napi::Object NewInstance(Napi::Env env, Napi::Value arg);
         static Napi::Object Init(Napi::Env env, Napi::Object exports);
         WrappedWord(const Napi::CallbackInfo &info);
-
+        /// get word form
+        Napi::Value GetForm(const Napi::CallbackInfo &info);
+        /// ~
+        /// Get word form, lowercased.
+        Napi::Value GetLcForm(const Napi::CallbackInfo &info);
+        /// ~
+        /// Get word phonetic form
+        Napi::Value GetPhForm(const Napi::CallbackInfo &info);
+        /// ~
+        /// get lemma for the selected analysis in list
+        Napi::Value GetLemma(const Napi::CallbackInfo &info);
+        /// ~
+        /// get tag for the selected analysis
+        Napi::Value GetTag(const Napi::CallbackInfo &info);
+        /// ~
+        /// true iff the word is a multiword compound
+        Napi::Value IsMultiword(const Napi::CallbackInfo &info);
 
     private:
         freeling::word* GetInternalInstance();
         static Napi::FunctionReference constructor;
         void createWrappedWord1arg(const Napi::CallbackInfo &info, Napi::Env env);
         void createWrappedWord2args(const Napi::CallbackInfo &info, Napi::Env env);
-
-        /// get word form
-        Napi::Value GetForm(const Napi::CallbackInfo &info);
-
-        /// ~
-        /// Get word form, lowercased.
-        Napi::Value GetLcForm(const Napi::CallbackInfo &info);
-
-        /// ~
-        /// Get word phonetic form
-        Napi::Value GetPhForm(const Napi::CallbackInfo &info);
-
-        /// ~
-        /// get lemma for the selected analysis in list
-        Napi::Value GetLemma(const Napi::CallbackInfo &info);
-
-        /// ~
-        /// get tag for the selected analysis
-        Napi::Value GetTag(const Napi::CallbackInfo &info);
-
-        /// ~
-        /// true iff the word is a multiword compound
-        Napi::Value IsMultiword(const Napi::CallbackInfo &info);
-
         freeling::word *word_;
     };
 }
