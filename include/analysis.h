3ce26b1309be3e6bfae401974e87d9e5b4ad9698
Leyla <lisana5632@gmail.com>
2019-02-16 21:23:37 +0300

    added Analysis class using AsyncWorker

--- include/analysis.h
+++ include/analysis.h
@@ -3,14 +3,52 @@
 
 #include <iostream>
 #include <napi.h>
-//#include <boost/lexical_cast.hpp>
 #include "freeling.h"
 #include "word.h"
 #include "whole.h"
 
 namespace freelingAddon {
-  Napi::Object GetWordAnalyses(Napi::Env env,freeling::word w);
-  Napi::Object GetAnalyses(const Napi::CallbackInfo& info);
+
+  Napi::Value EmptyCallback(const Napi::CallbackInfo& info);
+  Napi::Promise CallPromise(const Napi::CallbackInfo& info);
   Napi::Object InitWordAnalyses(Napi::Env env, Napi::Object exports);
+
+  class Analysis : public Napi::AsyncWorker {
+        public:
+            Analysis(Napi::Function& callback, Napi::Promise::Deferred deferred);
+            Napi::Object GetWordAnalyses(Napi::Env env);
+            freeling::word getAnalyzedWord();
+            void SetInputWord(std::wstring w);
+             ~Analysis();
+
+        protected:
+
+           virtual void Execute() override {
+             try {
+                analyzed_word=getAnalyzedWord();
+             }
+             catch(const Napi::TypeError &exc) {
+                deferred.Reject(exc.Value());
+             }
+          }
+
+          virtual void OnOK() override {
+              Napi::HandleScope scope(Env());
+              deferred.Resolve(GetWordAnalyses(Env()));
+              Callback().Call({});
+          }
+
+          virtual void OnError(const Napi::Error& e) override {
+              Napi::HandleScope scope(Env());
+              deferred.Reject(Napi::TypeError::New(Env(), "Promise can't be resolved").Value());
+              Callback().Call({});
+          }
+
+        private:
+            freeling::word analyzed_word;
+            std::wstring input_word;
+            Napi::Promise::Deferred deferred;
+  };
+
 };
 #endif
--- src/analysis.cpp
+++ src/analysis.cpp
@@ -1,24 +1,14 @@
 #include "analysis.h"
 
-Napi::Object freelingAddon::GetWordAnalyses(Napi::Env env,freeling::word w) {
-  Napi::Object result = Napi::Object::New(env);
-  result.Set("word",Napi::String::New(env,freeling::util::wstring2string(w.get_form())));
-  Napi::Array analyses =  Napi::Array::New(env);
-  size_t i=0;
-  for (freeling::word::iterator a=w.analysis_begin(); a!=w.analysis_end(); ++a) {
-      Napi::Object analysis = Napi::Object::New(env);
-      analysis.Set("lemma",Napi::String::New(env,freeling::util::wstring2string(a->get_lemma())));
-      analysis.Set("pos",Napi::String::New(env,freeling::util::wstring2string(a->get_tag())));
-      //std::string prob=boost::lexical_cast<std::string>(a->get_prob());
-      analysis.Set("prob",Napi::Number::New(env,a->get_prob()));
-      analyses.Set(i,analysis);
-      i++;
-  }
-  result.Set("analyses",analyses);
-  return result;
+freelingAddon::Analysis::Analysis(Napi::Function& callback, Napi::Promise::Deferred deferred):
+                                  Napi::AsyncWorker(callback),deferred(deferred){};
+
+void freelingAddon::Analysis::SetInputWord(std::wstring w) {
+    input_word=w;
 }
 
-freeling::word getAnalyzedWord(std::wstring w) { //just for test
+
+freeling::word freelingAddon::Analysis::getAnalyzedWord() {
       std::wstring lang = L"ru";
       freeling::util::init_locale(L"default");
       std::wstring ipath = L"/usr/local/share/freeling/";
@@ -26,39 +16,73 @@ freeling::word getAnalyzedWord(std::wstring w) { //just for test
       freeling::maco_options opt = freelingAddon::create_maco_opt(path, lang);
       freeling::splitter sp = freelingAddon::create_splitter(path);
       freeling::tokenizer tk = freelingAddon::create_tokenizer(path);
-      list<freeling::word> lw = tk.tokenize(w+L".");
+      list<freeling::word> lw = tk.tokenize(input_word+L".");
       list<freeling::sentence> ls = freelingAddon::morfo_an(opt, lw, sp);
       list<freeling::sentence>::iterator is=ls.begin();
       freeling::sentence::const_iterator ww=is->begin();
       return *ww;
 }
 
-Napi::Object freelingAddon::GetAnalyses(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
-  Napi::HandleScope scope(env);
-  try{
-      //if(!info.Length()) throw Napi::TypeError::New(env, "Required parameter is not provided");
-      if(info.Length()) {
-        if (info[0].IsObject()) {
-          Napi::Object object = info[0].As<Napi::Object>();
-          WrappedWord* wrapped_word = Napi::ObjectWrap<WrappedWord>::Unwrap(object);
-          Napi::String str = wrapped_word->GetForm(info).As<Napi::String>();
-          std::wstring form=freeling::util::string2wstring(str.Utf8Value());
-          return GetWordAnalyses(env,getAnalyzedWord(form));
+Napi::Object freelingAddon::Analysis::GetWordAnalyses(Napi::Env env) {
+      Napi::Object result = Napi::Object::New(env);
+      result.Set("word",Napi::String::New(env,freeling::util::wstring2string(analyzed_word.get_form())));
+      Napi::Array analyses =  Napi::Array::New(env);
+      size_t i=0;
+      for (freeling::word::iterator a=analyzed_word.analysis_begin(); a!=analyzed_word.analysis_end(); ++a) {
+          Napi::Object analysis = Napi::Object::New(env);
+          analysis.Set("lemma",Napi::String::New(env,freeling::util::wstring2string(a->get_lemma())));
+          analysis.Set("pos",Napi::String::New(env,freeling::util::wstring2string(a->get_tag())));
+          analysis.Set("prob",Napi::Number::New(env,a->get_prob()));
+          analyses.Set(i,analysis);
+          i++;
+      }
+      result.Set("analyses",analyses);
+      return result;
+}
+
+freelingAddon::Analysis::~Analysis(){};
+
+Napi::Value freelingAddon::EmptyCallback(const Napi::CallbackInfo& info){
+    Napi::Env env = info.Env();
+    Napi::HandleScope scope(env);
+    return env.Undefined();
+}
+
+
+Napi::Promise freelingAddon::CallPromise(const Napi::CallbackInfo& info) {
+    Napi::Env env = info.Env();
+    Napi::HandleScope scope(env);
+    Napi::Promise::Deferred deferred = Napi::Promise::Deferred::New(info.Env());
+    try{
+        if(info.Length()) {
+          if (info[0].IsObject() && info[0].As<Napi::Object>().InstanceOf(WrappedWord::constructor.Value())) {
+              Napi::Object object = info[0].As<Napi::Object>();
+              WrappedWord* wrapped_word = Napi::ObjectWrap<WrappedWord>::Unwrap(object);
+              Napi::String str = wrapped_word->GetForm(info).As<Napi::String>();
+              std::wstring form=freeling::util::string2wstring(str.Utf8Value());
+              Napi::Function callback = Napi::Function::New(env, EmptyCallback);
+              Analysis* worker = new Analysis(callback, deferred);
+              worker->SetInputWord(form);
+              worker->Queue();
+         }
+         else {
+           deferred.Reject(Napi::TypeError::New(env, "Argument must be an instance of Word").Value());
+         }
        }
-       else throw Napi::TypeError::New(env, "Argument must be an instance of Word");
-     }
-     else throw Napi::TypeError::New(env, "Required parameter is not provided");
-  }
-  catch(const Napi::TypeError &exc) {
-     exc.ThrowAsJavaScriptException();
-  }
-  catch( ... ) {
-      Napi::TypeError::New(env, DEFAULT_ERR_MSG).ThrowAsJavaScriptException();
-  }
+       else {
+          deferred.Reject(Napi::TypeError::New(env, "Required parameter is not provided").Value());
+       }
+    }
+    catch(const Napi::TypeError &exc) {
+       deferred.Reject(exc.Value());
+    }
+    catch( ... ) {
+        deferred.Reject(Napi::TypeError::New(env, DEFAULT_ERR_MSG).Value());
+    }
+    return deferred.Promise();
 }
 
 Napi::Object freelingAddon::InitWordAnalyses(Napi::Env env, Napi::Object exports) {
-    exports.Set("getAnalyses", Napi::Function::New(env, freelingAddon::GetAnalyses));
+    exports.Set("getAnalyses", Napi::Function::New(env, CallPromise));
     return exports;
 }
