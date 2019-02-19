910a8f7a7c55cb0ae635ff20fca04df646c2601c
Leyla <lisana5632@gmail.com>
2019-02-19 22:17:55 +0300

    added empty callback for async classes

--- src/addon_utils.cpp
+++ src/addon_utils.cpp
@@ -1,1 +1,7 @@
 #include "addon_utils.h"
+
+Napi::Value EmptyCallback(const Napi::CallbackInfo& info){
+    Napi::Env env = info.Env();
+    Napi::HandleScope scope(env);
+    return env.Undefined();
+}
