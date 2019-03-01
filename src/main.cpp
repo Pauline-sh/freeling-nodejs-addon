918c55f13bbc1a9dffdd55defe25404795bbd23e
Leyla <lisana5632@gmail.com>
2019-03-01 17:22:18 +0300

    removed extra lines

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "tokenizer.h"
 #include "splitter.h"
 #include "analysis.h"
-#include "pos_tagger.h"
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
 	freelingAddon::WrappedWord::Init(env, exports);
@@ -11,7 +10,7 @@ Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
     freelingAddon::WrappedSentence::Init(env, exports);
     freelingAddon::InitAsyncSplitter(env, exports);
     freelingAddon::InitAsyncTokenizer(env, exports);
-    return freelingAddon::InitAsyncPosTagger(env, exports);
+    return  freelingAddon::InitAsyncTokenizer(env, exports);
 }
 
 NODE_API_MODULE(freeling, InitAll)
