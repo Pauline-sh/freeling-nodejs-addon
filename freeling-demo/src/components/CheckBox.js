aaa6ad28d9f937c68fb79274074a9035ede33668
Leyla <lisana5632@gmail.com>
2019-03-27 21:19:00 +0300

    added Checkbox component

---
+++ freeling-demo/src/components/CheckBox.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class CheckBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={
+            checked:true
+        };
+        this.handleCheck=this.handleCheck.bind(this);
+    }
+    
+    handleCheck=(e)=>{
+        this.setState({checked: !this.state.checked});  
+    }
+    
+    render(){
+        return(
+            <div className="checkbox-wrapper">
+                <label className="checkbox-label">{this.props.label}
+                    <input type="checkbox" id={this.props.id} onChange={this.handleCheck} defaultChecked={this.state.checked}/>
+                    <span className="checkmark"></span>
+                </label>
+            </div>
+        );
+    };
+};
+
+
+export default CheckBox;
